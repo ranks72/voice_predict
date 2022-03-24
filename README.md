44d718c7641d12c6f75aaaad454e1e9c8ca7abee
ranks72 <ranksdead.id@gmail.com>
2022-03-24 21:10:41 +0700

    new file

--- README.md
+++ README.md
@@ -1,18 +1,1 @@
-# Movie Recommendation
-
-This is Final Project for Microcredential AI Certification Training by the Ministry of Research, Technology and Higher Education Indonesia. The dataset is collected through web-scraping meanwhile the recommendation system using K-means Clustering and Content-Based Filtering
-
-## About Microcredential AI Certification Training
-
-Microcredential AI Certification Training is a 6 weeks Data Science Training program for university students across Indonesia. The material consists of machine learning algorithms and data science tools including Jupyter Notebook, Python, and its libraries.
-
-## Deployment Link
-
-Website: https://movie-recommendation-microcred.herokuapp.com/
-
-## Reference
-
-https://www.imdb.com/search/title/?title_type=feature&num_votes=10000,&sort=user_rating,desc&ref_=adv_prv \
-https://dikti.kemdikbud.go.id/ \
-http://www.dikti.go.id/microcredential-ai/ \
-https://spadadikti.id/
+# voice_predict
--- main.py
+++ main.py
@@ -1,56 +1,106 @@
-from flask import *
-import pandas as pd
+from flask import Flask, jsonify, render_template, request
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+import python_speech_features
+import numpy as np
+import base64
+from pydub import AudioSegment
+import io
 
 app = Flask(__name__)
 
-# import dataset
-movie_data = pd.read_csv('machine_learning_model/IMDb_Movie_Dataset.csv', index_col=0)
-movie_rec_data = pd.read_csv('machine_learning_model/movie_recommendation_dataset.csv')
+word2index = {
+    # core words
+    "yes": 0,
+    "no": 1,
+    "up": 2,
+    "down": 3,
+    "left": 4,
+    "right": 5,
+    "on": 6,
+    "off": 7,
+    "stop": 8,
+    "go": 9,
+    "zero": 10,
+    "one": 11,
+    "two": 12,
+    "three": 13,
+    "four": 14,
+    "five": 15,
+    "six": 16,
+    "seven": 17,
+    "eight": 18,
+    "nine": 19,
+}
+index2word = [word for word in word2index]
 
-def get_recommendation(movie_name):
-    # find movie index
-    movie_index = movie_data.index[movie_data['Title'] == movie_name].values[0]
+def get_model():
+    global model
+    model = load_model("inggris.h5")
+    print("Model loaded")
 
-    # get list of movies recommendation
-    movies = movie_rec_data.iloc[movie_index]
-    movie_rec = []
-    for movie in movies:
-        temp = []
-        title = movie_data['Title'][movie]
-        year = movie_data['Year'][movie]
-        genre = movie_data['Genre'][movie]
-        rating = movie_data['Rating'][movie]
+def preprocess_audio(audio):
+    # audio = audio.astype(np.float)
+    # normalize data
+    audio=audio.astype('float32')
+    audio -= audio.mean()
+    audio /= np.max((audio.max(), -audio.min()))
+    # compute MFCC coefficients
+    features = python_speech_features.mfcc(audio, samplerate=16000, winlen=0.025, winstep=0.01, numcep=20, nfilt=40, nfft=512, lowfreq=100, highfreq=None, preemph=0.97, ceplifter=22, appendEnergy=True, winfunc=np.hamming)
+    return features
 
-        temp.append(title)
-        temp.append(year)
-        temp.append(genre)
-        temp.append(rating)
-        movie_rec.append(temp)
-    return movie_rec
+get_model()
 
-@app.route("/")
+@app.route("/predict", methods=["POST"])
+def predict():
+    message = request.get_json(force=True)
+    encoded = message['audio']
+    decoded = base64.b64decode(encoded)
+    audio = AudioSegment.from_file(io.BytesIO(decoded), format="wav")
+    print(audio)
+    #preprocess
+
+    samples = audio.get_array_of_samples()
+    samples = np.array(samples)
+
+    audio_preprocessed = preprocess_audio(samples)
+    recorded_feature = np.expand_dims(audio_preprocessed, 0)
+    prediction = model.predict(recorded_feature).reshape((20, ))
+    prediction /= prediction.sum()
+    #=======================================================
+    prediction_sorted_indices = prediction.argsort()
+
+    label = []
+    percentage = []
+    for k in range(3):
+        i = int(prediction_sorted_indices[-1-k])
+        label.append(index2word[i])
+        percentage.append(prediction[i]*100)
+        print("%d.)\t%s\t:\t%2.1f%%" % (k+1, label[k], percentage[k]))
+    
+    response = {
+        'prediction' : {
+            'label1' : label[0],
+            'prob1' : percentage[0],
+            'label2' : label[1],
+            'prob2' : percentage[1],
+            'label3' : label[2],
+            'prob3' : percentage[2]
+        }
+    }
+    #=======================================================
+    # response = {
+    #     "prediction" : prediction
+    # }
+
+    return jsonify(response)
+
+@app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template("home.html")
 
-@app.route('/search', methods=['GET', 'POST'])
-def search():
-    input_movie = request.form["input"]
-
-    # get input movie data
-    input_movie_data = []
-    input_movie_id = movie_data.index[movie_data['Title'] == input_movie].values[0]
-    title = movie_data['Title'][input_movie_id]
-    year = movie_data['Year'][input_movie_id]
-    genre = movie_data['Genre'][input_movie_id]
-    rating = movie_data['Rating'][input_movie_id]
-
-    input_movie_data.append(title)
-    input_movie_data.append(year)
-    input_movie_data.append(genre)
-    input_movie_data.append(rating)
-
-    # get movie recommendation
-    return render_template('home.html', movie_rec = get_recommendation(input_movie), input_movie_data = input_movie_data)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     # This is used when running locally only. When deploying to Google App
+#     # Engine, a webserver process such as Gunicorn will serve the app.
+#     app.run(host='127.0.0.1', port=8080, debug=True)
+# # [END gae_flex_quickstart]
