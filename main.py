0291c58cea4b2f00de391a09ea1f0173f7b35afa
Maliki Karim <47268573+ranks72@users.noreply.github.com>
2022-03-24 18:56:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -99,8 +99,8 @@ def predict():
 def home():
     return render_template("home.html")
 
-if __name__ == '__main__':
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app.
-    app.run(host='127.0.0.1', port=8080, debug=True)
-# [END gae_flex_quickstart]
+# if __name__ == '__main__':
+#     # This is used when running locally only. When deploying to Google App
+#     # Engine, a webserver process such as Gunicorn will serve the app.
+#     app.run(host='127.0.0.1', port=8080, debug=True)
+# # [END gae_flex_quickstart]
